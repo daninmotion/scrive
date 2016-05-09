247b3232861724c7bcf920b12ece37d7355cbe91
Daniel Cardenas <dan85.cardenas@gmail.com>
2016-05-08 23:21:07 -0400

    Infinite scrool

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,15 @@
 //= require turbolinks
 //= require_tree ../../../vendor/
 
+$(document).ready(function() {
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url = $('.pagination .next_page').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Loading...");
+        return $.getScript(url);
+      }
+    });
+    return $(window).scroll();
+  }
+});
